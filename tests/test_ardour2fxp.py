73e2cdf3ffb32984281ed2806ecde9309a4f5e2f
Christopher Arndt <chris@chrisarndt.de>
2021-01-15 01:21:57 +0100

    Make test more restant against output formatting changes; add more tests
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- tests/test_ardour2fxp.py
+++ tests/test_ardour2fxp.py
@@ -19,10 +19,12 @@ def sha1_digest(fn):
 
 
 @pytest.mark.parametrize("infn,plugin_id,label,sha1sum", [
-    ('vst-1331185229-single', 'OXFM', 'Nerf Pluck', '8609ff4e5d400487abfdbc10eeb97975a66b9f35'),
+    ('vst-1331185229-single', 'OXFM', 'Nerf Pluck',
+     'de90b1b924d74877d85248c0d5f5339741277372'),
 ])
-def test_chunkpreset_single(infn, plugin_id, sha1sum):
-    """Converting an ardour presets file with a single ChunkPreset produces correct output."""
+def test_chunkpreset_single(infn, plugin_id, label, sha1sum):
+    """Converting an ardour presets file with a single ChunkPreset produces
+       correct output."""
     infile = join(TESTDATA_DIR, infn)
     outdir = join(TESTOUTPUT_DIR, 'fxp')
     outfile = join(outdir, plugin_id, label.replace(' ', '_') + '.fxp')
@@ -34,17 +36,26 @@ def test_chunkpreset_single(infn, plugin_id, sha1sum):
 
     os.makedirs(outdir, exist_ok=True)
     ret = main(["-o", outdir, infile])
-
     assert ret is None
     assert exists(outfile)
     assert sha1_digest(outfile) == sha1sum
 
 
-@pytest.mark.parametrize("infn,plugin_id,labels", [
-    ('vst-1331185229', 'OXFM', ('Kick', 'Snare', 'INIT')),
+@pytest.mark.parametrize("infn,plugin_id,labels,sha1sums", [
+    (
+        'vst-1331185229',
+        'OXFM',
+        ('Kick', 'Snare', 'INIT'),
+        (
+            'b9c493fb4bd05e6b2167a852fd230204d438748a',
+            '942ed7564580f3cc2b40d0a02abe74aa4e6b177f',
+            '045ca4bd1e9ca874e2f187ce15dbb7b856942eec',
+        ),
+    ),
 ])
-def test_chunkpreset_single(infn, plugin_id, labels):
-    """Converting an ardour presets file with multiple ChunkPresets produces correct output."""
+def test_chunkpreset_multi(infn, plugin_id, labels, sha1sums):
+    """Converting an ardour presets file with multiple ChunkPresets produces
+       correct output."""
     infile = join(TESTDATA_DIR, infn)
     outdir = join(TESTOUTPUT_DIR, 'fxp')
 
@@ -59,16 +70,19 @@ def test_chunkpreset_single(infn, plugin_id, labels):
     ret = main(["-o", outdir, infile])
     assert ret is None
 
-    for label in labels:
+    for i, label in enumerate(labels):
         outfile = join(outdir, plugin_id, label.replace(' ', '_') + '.fxp')
         assert exists(outfile)
+        assert sha1_digest(outfile) == sha1sums[i]
 
 
 @pytest.mark.parametrize("infn,plugin_id,label,sha1sum", [
-    ('vst-1466847281', 'WnP1', 'Drum Reverb', '1af7a14d82eb2d87be608126ea4716cd722b86d8'),
+    ('vst-1466847281', 'WnP1', 'Drum Reverb',
+     '1af7a14d82eb2d87be608126ea4716cd722b86d8'),
 ])
-def test_parampreset(infn, plugin_id, label, sha1sum):
-    """Converting an ardour presets file with a single ChunkPresets produces correct output."""
+def test_parampreset_single(infn, plugin_id, label, sha1sum):
+    """Converting an ardour presets file with a single parameter Preset produces
+       correct output."""
     infile = join(TESTDATA_DIR, infn)
     outdir = join(TESTOUTPUT_DIR, 'fxp')
     outfile = join(outdir, plugin_id, label.replace(' ', '_') + '.fxp')
@@ -83,3 +97,40 @@ def test_parampreset(infn, plugin_id, label, sha1sum):
     assert ret is None
     assert exists(outfile)
     assert sha1_digest(outfile) == sha1sum
+
+
+# TODO: find or create real-life example files for this test
+@pytest.mark.skip()
+@pytest.mark.parametrize("infn,plugin_id,labels,sha1sums", [
+    (
+        'vst-1234567890',
+        'ABCD',
+        ('Preset 1', 'Preset 2', 'Preset 3'),
+        (
+            'abcdef1234567890abcdef1234567890abcdef12',
+            'abcdef1234567890abcdef1234567890abcdef12',
+            'abcdef1234567890abcdef1234567890abcdef12',
+        ),
+    ),
+])
+def test_parampreset_multi(infn, plugin_id, labels, sha1sums):
+    """Converting an ardour presets file with multiple parameter Presets produces
+       correct output."""
+    infile = join(TESTDATA_DIR, infn)
+    outdir = join(TESTOUTPUT_DIR, 'fxp')
+
+    for label in labels:
+        outfile = join(outdir, plugin_id, label.replace(' ', '_') + '.fxp')
+        try:
+            os.remove(outfile)
+        except OSError:
+            pass
+
+    os.makedirs(outdir, exist_ok=True)
+    ret = main(["-o", outdir, infile])
+    assert ret is None
+
+    for i, label in enumerate(labels):
+        outfile = join(outdir, plugin_id, label.replace(' ', '_') + '.fxp')
+        assert exists(outfile)
+        assert sha1_digest(outfile) == sha1sums[i]
--- tests/test_fxp2ardour.py
+++ tests/test_fxp2ardour.py
@@ -3,8 +3,10 @@
 
 import hashlib
 import os
+import shutil
 
 from os.path import dirname, join, exists
+from xml.dom import minidom
 import pytest
 
 from fxp2ardour import main
@@ -18,8 +20,21 @@ def sha1_digest(fn):
         return hashlib.sha1(fp.read()).hexdigest()
 
 
+def check_output(fn):
+    """Reformat given XML file with xml.dom.minidom and return SHA1 checksum of
+       resulting file.
+    """
+    xml = minidom.parse(fn)
+
+    with open(fn, 'wb') as fp:
+        fp.write(xml.toprettyxml(indent='  ', encoding='utf-8'))
+
+    return sha1_digest(fn)
+
+
 @pytest.mark.parametrize("infn,outfn,sha1sum", [
-    ('MDAx_Harp_FxCk.fxp', 'vst-1296318840', '8609ff4e5d400487abfdbc10eeb97975a66b9f35'),
+    ('MDAx_Harp_FxCk.fxp', 'vst-1296318840',
+     '7af1e786bf94bbecda30fdde3c791ceb56da50f4'),
 ])
 def test_fxp_fxck(infn, outfn, sha1sum):
     """Converting an FXP file of type FxCk produces correct output."""
@@ -37,11 +52,12 @@ def test_fxp_fxck(infn, outfn, sha1sum):
 
     assert ret is None
     assert exists(outfile)
-    assert sha1_digest(outfile) == sha1sum
+    assert check_output(outfile) == sha1sum
 
 
 @pytest.mark.parametrize("infn,outfn,sha1sum", [
-    ('OXFM_GlassyEPiano_FPCh.fxp', 'vst-1331185229', 'aa90cb4a2ef6a12b701e3e0eeda8f2d315ef551b'),
+    ('OXFM_GlassyEPiano_FPCh.fxp', 'vst-1331185229',
+     '30db17c08c70fd75aebce52e8193cb56b251f0bd'),
 ])
 def test_fxp_fpch(infn, outfn, sha1sum):
     """Converting an FXP file of type FPCh produces correct output."""
@@ -59,4 +75,54 @@ def test_fxp_fpch(infn, outfn, sha1sum):
 
     assert ret is None
     assert exists(outfile)
-    assert sha1_digest(outfile) == sha1sum
+    assert check_output(outfile) == sha1sum
+
+
+@pytest.mark.parametrize("infn,outfn,sha1sum", [
+    ('OXFM_GlassyEPiano_FPCh.fxp', 'vst-1331185229',
+     'b40bf52a84811379dda0cac81ba292bf9fd97e15'),
+])
+def test_fxp_merge(infn, outfn, sha1sum):
+    """Converting and merging an FXP file produces correct output."""
+    infile = join(TESTDATA_DIR, infn)
+    infile2 = join(TESTDATA_DIR, outfn)
+    outdir = join(TESTOUTPUT_DIR, 'ardour')
+    outfile = join(outdir, outfn)
+
+    try:
+        os.remove(outfile)
+    except OSError:
+        pass
+
+    os.makedirs(outdir, exist_ok=True)
+    shutil.copyfile(infile2, outfile)
+    ret = main(["-o", outdir, "-m", infile])
+
+    assert ret is None
+    assert exists(outfile)
+    assert check_output(outfile) == sha1sum
+
+
+@pytest.mark.parametrize("infn,outfn,sha1sum", [
+    ('OXFM_Kick_FPCh.fxp', 'vst-1331185229',
+     '93c6dd14c89e5ff5d5d27d01930fdf05f898b968'),
+])
+def test_fxp_append(infn, outfn, sha1sum):
+    """Converting and appending an FXP file produces correct output."""
+    infile = join(TESTDATA_DIR, infn)
+    infile2 = join(TESTDATA_DIR, outfn)
+    outdir = join(TESTOUTPUT_DIR, 'ardour')
+    outfile = join(outdir, outfn)
+
+    try:
+        os.remove(outfile)
+    except OSError:
+        pass
+
+    os.makedirs(outdir, exist_ok=True)
+    shutil.copyfile(infile2, outfile)
+    ret = main(["-o", outdir, "-a", infile])
+
+    assert ret is None
+    assert exists(outfile)
+    assert check_output(outfile) == sha1sum
